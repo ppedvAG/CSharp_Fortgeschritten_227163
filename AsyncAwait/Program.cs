032f78fad7367d2afe5a89a9a0643586d44b904f
ppedvLukasK <lukask@ppedv.de>
2023-06-06 16:49:14 +0200

    M008

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Diagnostics;
+
+namespace AsyncAwait;
+
+internal class Program
+{
+	static async Task Main(string[] args)
+	{
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Toast();
+		//Geschirr();
+		//Kaffee();
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //7s
+
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Task t1 = Task.Run(Toast);
+		//Task t2 = Task.Run(Geschirr);
+		//t2.Wait();
+		//Task t3 = Task.Run(Kaffee);
+		//Task.WaitAll(t1, t3);
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds); //4s aber Wait
+
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Task t1 = Task.Run(Toast);
+		//Task t2 = Task.Run(Geschirr);
+		//Task t3 = null;
+		//t2.ContinueWith(t => t3 = Task.Run(Kaffee));
+		//t1.ContinueWith(t =>
+		//{
+		//	sw.Stop();
+		//	Console.WriteLine(sw.ElapsedMilliseconds); //4s aber Wait
+		//});
+		//Console.ReadKey();
+
+		//async Methoden:
+		//Enden mit Async laut Konvention, wenn sie awaiten werden sollen
+		//async void: Kann selbst await verwenden, kann aber nicht awaited werden
+		//async Task: Kann selbst await verwenden und awaited werden
+		//async Task<T>: Kann selbst await verwenden, awaited werden und gibt ein Objekt als Ergebnis zurück
+		//Wenn eine Async Methode gestartet wird, wird sie als Task gestartet
+		//await: Warte darauf das dieser Task fertig ist
+
+		//Stopwatch sw = Stopwatch.StartNew();
+		//Task t1 = ToastAsync();
+		//Task t2 = GeschirrAsync();
+		//await t2;
+		//Task t3 = KaffeeAsync();
+		//await t1;
+		//await t3;
+		//sw.Stop();
+		//Console.WriteLine(sw.ElapsedMilliseconds);
+
+		//await bei Tasks mit Rückgabewert gibt das Objekt des Tasks zurück -> t.Result
+		Stopwatch sw = Stopwatch.StartNew();
+		Task<Toast> t1 = ToastObjectAsync();
+		Task<Geschirr> t2 = GeschirrObjectAsync();
+		Geschirr g = await t2;
+		Task<Kaffee> t3 = KaffeeObjectAsync(g);
+		Toast t = await t1;
+		Kaffee k = await t3;
+		sw.Stop();
+		Console.WriteLine(sw.ElapsedMilliseconds);
+		Console.ReadLine();
+
+		//Task<Toast> t1 = ToastObjectAsync();
+		//Task<Kaffee> t3 = KaffeeObjectAsync(await GeschirrObjectAsync());
+		//await Task.WhenAll(t1, t3);
+
+		//WhenAll: Warte auf mehrere Tasks, nützlich bei Tasks bei denen nicht klar ist wie lang diese dauern werden
+		await Task.WhenAll(t1, t2, t3); //-> mehrere await Statements konsolidieren
+		await Task.WhenAny(t1, t2, t3); //WhenAny: Warte auf den ersten Task
+	}
+
+	static void Toast()
+	{
+		Thread.Sleep(4000);
+		Console.WriteLine("Toast fertig");
+	}
+
+	static void Geschirr()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Geschirr fertig");
+	}
+
+	static void Kaffee()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Kaffee fertig");
+	}
+
+	static async Task ToastAsync()
+	{
+		await Task.Delay(4000); //await: Warte darauf das dieser Task fertig ist
+		Console.WriteLine("Toast fertig");
+		//return ist nicht notwendig bei Task ohne Generic als Rückgabetyp
+	}
+
+	static async Task GeschirrAsync()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Geschirr fertig");
+	}
+
+	static async Task KaffeeAsync()
+	{
+		Thread.Sleep(1500);
+		Console.WriteLine("Kaffee fertig");
+	}
+
+	static async Task<Toast> ToastObjectAsync()
+	{
+		await Task.Delay(4000); //await: Warte darauf das dieser Task fertig ist
+		Console.WriteLine("Toast fertig");
+		return new Toast();
+	}
+
+	static async Task<Geschirr> GeschirrObjectAsync()
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Geschirr fertig");
+		return new Geschirr();
+	}
+
+	static async Task<Kaffee> KaffeeObjectAsync(Geschirr g)
+	{
+		await Task.Delay(1500);
+		Console.WriteLine("Kaffee fertig");
+		return new Kaffee();
+	}
+}
+
+public class Toast { }
+
+public class Geschirr { }
+
+public class Kaffee { }
---
+++ AsyncAwaitWPF/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitWPF;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			Thread.Sleep(25);
+			Progress.Value++;
+		} //UI Updates finden am Main Thread statt, Main Thread wird blockiert
+	}
+
+	private void Button_Click1(object sender, RoutedEventArgs e)
+	{
+		Task.Run(() => //UI Updates sind nicht erlaubt von Side Threads/Tasks
+		{
+			Dispatcher.Invoke(() => Progress.Value = 0); //Dispatcher.Invoke um UI Updates von Side Threads/Tasks zu machen
+			for (int i = 0; i < 100; i++)
+			{
+				Thread.Sleep(25);
+				Dispatcher.Invoke(() => Progress.Value++);
+			}
+		});
+	}
+
+	private async void Button_Click2(object sender, RoutedEventArgs e)
+	{
+		Progress.Value = 0;
+		for (int i = 0; i < 100; i++)
+		{
+			await Task.Delay(25);
+			Progress.Value++;
+		}
+	}
+
+	private async void Button_Click3(object sender, RoutedEventArgs e)
+	{
+		using HttpClient client = new HttpClient();
+		Task<HttpResponseMessage> responseTask = client.GetAsync(@"http://www.gutenberg.org/files/54700/54700-0.txt"); //Hier wird der Task gestartet
+		TB.Text = "Text wird geladen..."; //Dinge vor dem Ergebnis machen
+		HttpResponseMessage resp = await responseTask; //Warte hier auf das Ergebnis
+		if (resp.IsSuccessStatusCode)
+		{
+			Task<string> stringTask = resp.Content.ReadAsStringAsync();
+			string text = await stringTask;
+			TB.Text = text;
+		}
+	}
+
+	private async void Button_Click4(object sender, RoutedEventArgs e)
+	{
+		using HttpClient client = new HttpClient();
+		Task<HttpResponseMessage> responseTask = client.GetAsync(@"http://www.gutenberg.org/files/54700/54700-0.txt"); //Hier wird der Task gestartet
+		TB.Text = "Text wird geladen..."; //Dinge vor dem Ergebnis machen
+		HttpResponseMessage resp = await responseTask; //Warte hier auf das Ergebnis
+		if (resp.IsSuccessStatusCode)
+		{
+			Task<string> stringTask = resp.Content.ReadAsStringAsync();
+			string text = await stringTask;
+
+			string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //Pfad zum Desktop
+			string folderPath = System.IO.Path.Combine(desktop, "Test");
+			string filePath = System.IO.Path.Combine(folderPath, "Test");
+
+			if (!Directory.Exists(folderPath))
+				Directory.CreateDirectory(folderPath);
+
+			ConcurrentDictionary<int, string> texte = new();
+			for (int i = 0; i < 100; i++)
+			{
+				texte.TryAdd(i, text + text + text + text + text + text + text + text + text + text);
+			}
+
+			long foreachMS;
+
+			Stopwatch sw = Stopwatch.StartNew();
+			foreach (KeyValuePair<int, string> t in texte)
+			{
+				await File.WriteAllTextAsync(filePath + $"{t.Key}.txt", t.Value);
+				TB.Text = $"File {t.Key} geschrieben";
+				Progress.Value++;
+			}
+			sw.Stop();
+			foreachMS = sw.ElapsedMilliseconds;
+
+			Progress.Value = 0;
+			sw.Restart();
+			await Parallel.ForEachAsync(texte, (t, ct) =>
+			{
+				File.WriteAllText(filePath + $"{t.Key + 100}.txt", t.Value);
+				Dispatcher.Invoke(() =>
+				{
+					TB.Text = $"File {t.Key + 100} geschrieben";
+				});
+				return ValueTask.CompletedTask;
+			});
+			sw.Stop();
+			TB.Text = foreachMS.ToString() + "\n" + sw.ElapsedMilliseconds.ToString();
+		}
+	}
+}
